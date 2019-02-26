b9f1133aa1deabe17785a5fca37952d53d3b2b50
Abhinav Jha <abjha@mediamath.com>
2019-02-25 21:20:22 -0500

    Updates

---
+++ precache-manifest.fbcb1022dd402b9dd3e9ef188c65b20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bf3c7d9dd96407cf10e",
+    "url": "/portfolio/static/css/main.ffc1949a.chunk.css"
+  },
+  {
+    "revision": "4bf3c7d9dd96407cf10e",
+    "url": "/portfolio/static/js/main.4bf3c7d9.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/portfolio/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "84b12ab08eb81622b4674fe635d9b228",
+    "url": "/portfolio/index.html"
+  }
+];
